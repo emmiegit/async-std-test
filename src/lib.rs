f49bad19cb1fcccf6ba55953de3e252b6244f8eb
Ammon Smith <ammon.i.smith@gmail.com>
2022-04-30 02:17:38 -0400

    Create initial async_test() based on async-attributes.

--- src/lib.rs
+++ src/lib.rs
@@ -11,4 +11,36 @@
  *
  */
 
-// TODO
+#![forbid(unsafe_code, future_incompatible)]
+#![deny(missing_debug_implementations, nonstandard_style)]
+
+use proc_macro::TokenStream;
+use quote::{quote, quote_spanned};
+use syn::spanned::Spanned;
+
+#[proc_macro_attribute]
+pub fn async_test(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let input = syn::parse_macro_input!(item as syn::ItemFn);
+
+    let ret = &input.sig.output;
+    let name = &input.sig.ident;
+    let body = &input.block;
+    let attrs = &input.attrs;
+    let vis = &input.vis;
+
+    if input.sig.asyncness.is_none() {
+        return TokenStream::from(quote_spanned! { input.span() =>
+            compile_error!("the async keyword is missing from the function declaration"),
+        });
+    }
+
+    let result = quote! {
+        #[::core::prelude::v1::test]
+        #(#attrs)*
+        #vis fn #name() #ret {
+            async_std::task::block_on(async { #body })
+        }
+    };
+
+    result.into()
+}
